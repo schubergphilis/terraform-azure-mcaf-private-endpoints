194adc30b1816d820b20824fc08ae8d5787e52d3
Sander de Vries <sdevries@schubergphilis.com>
2024-10-10 15:30:06 +0200

    remove unused variable

--- variables.tf
+++ variables.tf
@@ -125,9 +125,3 @@ variable "resource_group_name" {
   description = "The name of the resource group in which to create the virtual network."
   type        = string
 }
-
-variable "tags" {
-  description = "A map of tags to assign to the resource."
-  type        = map(string)
-  default     = {}
-}

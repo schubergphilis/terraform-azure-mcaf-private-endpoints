c17a76e4e829cdce55ce4e2dad2afd66118ee09d
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-06 16:01:00 +0100

    variable names were changed so now main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_private_endpoint" "this" {
   for_each = var.private_endpoints
 
-  name                          = each.value.name != null ? each.value.name : "${each.key}_pep"
+  name                          = each.value.name != null ? each.value.name : "${regex("([^/]+)$", each.value.private_connection_resource_id)[0]}-pep"
   location                      = var.location
   resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
-  subnet_id                     = each.value.subnet_id
+  subnet_id                     = each.value.subnet_resource_id
   custom_network_interface_name = each.value.custom_network_interface_name != null ? each.value.custom_network_interface_name : "${regex("([^/]+)$", each.value.private_connection_resource_id)[0]}-nic"
 
   private_service_connection {

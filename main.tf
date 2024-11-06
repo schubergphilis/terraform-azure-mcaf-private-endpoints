c885852546b646729264b3e1ea27b41e1b11db8e
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-06 16:15:30 +0100

    adding extra name for subresource

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_private_endpoint" "this" {
   for_each = var.private_endpoints
 
-  name                          = each.value.name != null ? each.value.name : "${regex("([^/]+)$", each.value.private_connection_resource_id)[0]}-pep"
+  name                          = each.value.name != null ? each.value.name : "${regex("([^/]+)$", each.value.private_connection_resource_id)[0]}-${each.value.subresource_name}-pep"
   location                      = var.location
   resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
   subnet_id                     = each.value.subnet_id

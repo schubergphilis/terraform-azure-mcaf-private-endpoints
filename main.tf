3124d522f248a6005b9976d0c716644742d23d12
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-06 20:37:12 +0100

    remove old values and fix name

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 resource "azurerm_private_endpoint" "this" {
   for_each = var.private_endpoints
 
-  name                          = each.value.name != null ? each.value.name : "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-pep"
-  #name                          = each.value.name != null ? each.value.name : "${regex("([^/]+)$", each.value.private_connection_resource_id)[0]}-${each.value.subresource_name}-pep"
+  name                          = each.value.name != null ? each.value.name : "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-${each.value.subresource_name}-pep"
   location                      = var.location
   resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
   subnet_id                     = each.value.subnet_id
-  #custom_network_interface_name = each.value.custom_network_interface_name != null ? each.value.custom_network_interface_name : "${regex("([^/]+)$", each.value.private_connection_resource_id)[0]}-nic"
   custom_network_interface_name = each.value.custom_network_interface_name != null ? each.value.custom_network_interface_name : "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-nic"
 
   private_service_connection {

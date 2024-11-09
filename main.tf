2d1c53430c695cb31346df02fc4705ff056be83f
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-09 13:42:14 +0100

    change functions for the private endpoint

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_private_endpoint" "this" {
   for_each = var.private_endpoints
 
-  name                          = each.value.name != null ? each.value.name : "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-${each.value.subresource_name}-pep"
-  location                      = try(each.value.location,  each.value.location, null)
-  resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
+  name                          = try(each.value.name, "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-${each.value.subresource_name}-pep")
+  location                      = coalesce(each.value.location, var.location)
+  resource_group_name           = coalesce(each.value.resource_group_name, var.resource_group_name)
   subnet_id                     = each.value.subnet_id
-  custom_network_interface_name = each.value.custom_network_interface_name != null ? each.value.custom_network_interface_name : "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-nic"
+  custom_network_interface_name = try(each.value.custom_network_interface_name, "${provider::azurerm::parse_resource_id(each.value.private_connection_resource_id)["resource_name"]}-nic")
 
   private_service_connection {
     name                              = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "${each.key}_psc"

31459b6a351e33040990c80ed9bd7c1c78ac70c5
Marius Waldhorst <mwaldhorst@schubergphilis.com>
2025-07-25 13:48:31 +0200

    Breaking: Change output to return endpoints by the same key they are referenced in the input

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "private_endpoint_list" {
+output "private_endpoints" {
   description = "A map of private endpoint names to their corresponding names and IDs"
   value = merge(
     {
-      for private_endpoint in azurerm_private_endpoint.this : private_endpoint.name => {
+      for name, private_endpoint in azurerm_private_endpoint.this : name => {
         name = private_endpoint.name
         id   = private_endpoint.id
         ip   = private_endpoint.private_service_connection[0].private_ip_address
       }
     },
     {
-      for private_endpoint in azurerm_private_endpoint.this_unmanaged_dns_zone_groups : private_endpoint.name => {
+      for name, private_endpoint in azurerm_private_endpoint.this_unmanaged_dns_zone_groups : name => {
         name = private_endpoint.name
         id   = private_endpoint.id
         ip   = private_endpoint.private_service_connection[0].private_ip_address
